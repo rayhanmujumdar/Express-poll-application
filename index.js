96650c097fab958e9ea196a2a4145b101899542d
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-09-25 01:42:13 +0600

    root file change

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ app.set("view engine", "ejs");
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.get('/',(req,res)=> {
+  res.json({message: "this our root file"})
+})
+// app.get("/", (req, res) => {
+//   res.render("home");
+// });
 app.post("/create", createPollPostController);
 app.get("/create", createPollGetController);
 app.get("/polls", getAllPolls);
